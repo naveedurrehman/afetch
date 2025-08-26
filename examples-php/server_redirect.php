7db1fb9d39a622298d17353c49d693ab9c44d27d
Naveed ur Rehman <naveed.ur.rehman@hotmail.com>
2025-08-26 23:51:23 +1200

    api moved to naveedurrehman

--- examples-php/server_redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/javascript');
-$url = ['https://www.google.com', 'https://www.yahoo.com'][rand(0, 1)];
-echo <<<TXT
-
-    window.location="$url";
-    
-TXT;
--- examples-php/server_slow.php
+++
@@ -1,3 +0,0 @@
-<?php
-sleep(3);   //intentional delay in response
-echo date("j F Y h:isa");
